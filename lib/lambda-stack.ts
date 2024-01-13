11be9b6ae596d137f6343f777a9acff303f1267b
jouinfogix <jou@infogix.com>
2024-01-13 00:19:08 -0600

    Add testing Stage

---
+++ lib/lambda-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as lambda  from 'aws-cdk-lib/aws-lambda';
+
+export class lambdaStack extends cdk.Stack {
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+      super(scope, id, props);
+  
+      const demolambda = new lambda.Function(this, 'LambdaFunction', {
+        runtime: lambda.Runtime.NODEJS_18_X,
+        handler: 'index.handler',
+        functionName: 'demo-jingsong',
+        code: lambda.Code.fromInline('exports.handler = _ => "Hello, CDK";')
+      });
+    }
+}

e816a21af536e6b52ac4febcc2994315c1fb235e
jouinfogix <jou@infogix.com>
2024-01-12 23:33:27 -0600

    initial commit

---
+++ lib/demoawspipeline-stack.ts
@@ -0,0 +1,29 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+// import { PipelineAppStage } from './demoawspipeline-app-stack';
+// import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
+
+
+export class DemoawspipelineStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+    // AWS CI-CD Pipeline
+    const democicdpipeline = new CodePipeline(this,'demopipeline',
+{
+      synth: new ShellStep('Synth', {
+        // Use a connection created using the AWS console to authenticate to GitHub
+        // Other sources are available.
+        input: CodePipelineSource.gitHub('jouinfogix/democicd011224', 'main'),
+        commands: [
+          'npm ci',
+          'npm run build',
+          'npx cdk synth',
+        ],
+      }),
+    });
+
+  }
+}
